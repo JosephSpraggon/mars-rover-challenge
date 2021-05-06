53240af5f63f2b0cded73e465d412d716f5d771d
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-05-06 16:11:55 +0100

    refactored the rover class to take arguements

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,9 +1,9 @@
 class Rover
   attr_reader :start_postion, :plateau_area
 
-  def initialize
-    @start_postion = "1 2 N"
-    @plateau_area = "5 5"
+  def initialize(start_postion, plateau_area)
+    @start_postion = start_postion
+    @plateau_area = plateau_area
   end
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,13 +1,15 @@
 require 'rover'
 
-describe Rover do
-  it 'can create a rover object' do
-    expect(subject).to be_an_instance_of(Rover)
-  end
+  test_rover = Rover.new('1 2 N', '5 5')
 
-  it 'is initializes rover objects with starting position and the plateau area' do
-    expect(subject.start_postion).to eq('1 2 N')
-    expect(subject.plateau_area).to eq('5 5')
-  end
+  describe Rover do
+    it 'can create a rover object' do
+      expect(test_rover).to be_an_instance_of(Rover)
+    end
+
+    it 'is initializes rover objects with starting position and the plateau area' do
+      expect(test_rover.start_postion).to eq('1 2 N')
+      expect(test_rover.plateau_area).to eq('5 5')
+    end
 
 end
