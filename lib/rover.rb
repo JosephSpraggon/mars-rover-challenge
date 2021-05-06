3e37af7ea326ea052b67259e03f90422d924b181
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-05-06 15:55:04 +0100

    second passing test, rover initialize with a starting point & the plateau area

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,3 +1,9 @@
 class Rover
+  attr_reader :start_postion, :plateau_area
+
+  def initialize
+    @start_postion = "1 2 N"
+    @plateau_area = "5 5"
+  end
 
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -5,4 +5,9 @@ describe Rover do
     expect(subject).to be_an_instance_of(Rover)
   end
 
+  it 'is initializes rover objects with starting position and the plateau area' do
+    expect(subject.start_postion).to eq('1 2 N')
+    expect(subject.plateau_area).to eq('5 5')
+  end
+
 end
